92302f8c5365b844c386394a2b755b49f2e08df1
Richard Fenix <richard.fenix@gmail.com>
2018-07-19 04:21:42 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears 
+
+Mr. Chocolate
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
+unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+PART 3: SELECT
