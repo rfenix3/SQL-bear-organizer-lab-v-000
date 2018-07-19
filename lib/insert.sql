eb722ef62aef5e6051787f5a2419b29fffd74efc
Richard Fenix <richard.fenix@gmail.com>
2018-07-19 04:39:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,20 +1,11 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Mr. Chocolate', 7, 'M', 'brown', 'friendly');
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Rowdy', 5, 'M', 'black', 'aggressive');
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Tabitha', 6, 'F', 'black', 'laid back');
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Sergeant Brown', 9, 'M', 'white', 'aggressive');
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Sergeant Brown', 9, 'M', 'white', 'aggressive');
-
-Melissa 'laid back'
-Grinch 'aggressive'
-Wendy 'friendly'
-unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
-PART 3: SELECT
+VALUES 
+  ('Mr. Chocolate', 7, 'M', 'brown', 'friendly'), 
+  ('Rowdy', 5, 'M', 'black', 'aggressive'), 
+  ('Tabitha', 6, 'F', 'black', 'laid back'), 
+  ('Sergeant Brown', 9, 'M', 'white', 'aggressive'), 
+  ('Melissa', 8, 'F', 'white', 'friendly'),
+  ('Grinch', 15, 'M', 'green', 'aggressive'),
+  ('Wendy', 10, 'F', 'brown', 'friendly'),
+  (NULL, 12, 'F', 'white', 'aggressive');
+  
