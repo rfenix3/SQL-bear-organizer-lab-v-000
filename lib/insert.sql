abc1bbd4e30442733c51844482964adbfa2c17aa
Richard Fenix <richard.fenix@gmail.com>
2018-07-19 04:42:48 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
 VALUES 
-  ('Mr. Chocolate', 7, 'M', 'brown', 'friendly'), 
-  ('Rowdy', 5, 'M', 'black', 'aggressive'), 
-  ('Tabitha', 6, 'F', 'black', 'laid back'), 
-  ('Sergeant Brown', 9, 'M', 'white', 'aggressive'), 
+  ('Mr. Chocolate', 7, 'M', 'brown', 'friendly', 1), 
+  ('Rowdy', 5, 'M', 'black', 'aggressive', 1), 
+  ('Tabitha', 6, 'F', 'black', 'laid back', 1), 
+  ('Sergeant Brown', 15, 'M', 'white', 'aggressive', 0), 
   ('Melissa', 8, 'F', 'white', 'friendly'),
   ('Grinch', 15, 'M', 'green', 'aggressive'),
   ('Wendy', 10, 'F', 'brown', 'friendly'),
