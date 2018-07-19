b2316866e4e3202ea78b0bdec5b04e27e3bf9c7f
Richard Fenix <richard.fenix@gmail.com>
2018-07-19 04:30:37 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,11 +3,11 @@ VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'friendly');
 
 
 Mr. Chocolate 'friendly'
-Rowdy 'agressive'
+Rowdy 'aggressive'
 Tabitha 'laid back'
 Sergeant Brown 'agressive'
 Melissa 'laid back'
-Grinch 'agressive'
+Grinch 'aggressive'
 Wendy 'friendly'
 unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
 PART 3: SELECT
