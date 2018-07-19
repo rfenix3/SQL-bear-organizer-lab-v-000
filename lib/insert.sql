b5f929a48b929a7e4138a9505fce9231764caebc
Richard Fenix <richard.fenix@gmail.com>
2018-07-19 04:29:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'laid back');
+VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'friendly');
 
 
-Mr. Chocolate 'laid back'
+Mr. Chocolate 'friendly'
 Rowdy 'agressive'
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
+Tabitha 'laid back'
+Sergeant Brown 'agressive'
+Melissa 'laid back'
+Grinch 'agressive'
+Wendy 'friendly'
 unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
 PART 3: SELECT
